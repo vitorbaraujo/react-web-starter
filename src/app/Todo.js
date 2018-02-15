c745564d0292165c61cb7a1f3196e2293a1c174d
Vitor Barbosa <vitornga15@gmail.com>
2018-02-14 22:55:37 -0200

    [todo-exercise] Add method to remove todos

--- src/app/Todo.js
+++ src/app/Todo.js
@@ -9,10 +9,14 @@ export class Todo extends Component {
   }
 
   handleClick(e) {
-    console.log('oi')
     e.preventDefault();
     const todos = [ ...this.state.todos, this.input.value ];
     this.input.value = '';
+    this.setState({ todos })
+  }
+
+  removeTodo(idx) {
+    const todos = [...this.state.todos.slice(0, idx), ...this.state.todos.slice(idx + 1)]
     this.setState({ todos })
   }
 
@@ -24,7 +28,9 @@ export class Todo extends Component {
           <button onClick={this.handleClick.bind(this)}>create</button>
         </form>
         <ul>
-          {this.state.todos.map((todo, idx) => <li key={idx}>{todo}</li>)}
+          {this.state.todos.map((todo, idx) => (
+            <li key={idx} onClick={() => this.removeTodo.call(this, idx)}>{todo}</li>
+          ))}
         </ul>
       </div>
     );
